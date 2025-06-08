fb1709ce5bae4db8a3270c82b90a803675c1d89b
Hichem BEL-HADJ <hichem.belhadj7@gmail.com>
2025-06-08 20:50:10 +0200

    Configuration I18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TuiRoot} from '@taiga-ui/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [TuiRoot],
+  imports: [TuiRoot, TranslatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
